8f370075d5f67064e4a3b9e41e2d782a29498a28
kathirmprofessional-afk <kathir.m.professional@gmail.com>
2026-03-12 15:13:16 +0530

    refactor: restore original React frontend UI

--- chess_tournament/urls.py
+++ chess_tournament/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts.views import api_login_view, api_register_view
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login', api_login_view, name='api_login'),
@@ -9,6 +11,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('tournament/', include('tournaments.urls')),
     path('match/', include('matches.urls')),
-    path('', lambda request: __import__('django.shortcuts', fromlist=['redirect']).redirect('dashboard'), name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
